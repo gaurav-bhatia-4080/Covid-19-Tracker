2b35d70dd4cd29895a2e7d85ad3a463cdcc29248
gaurav-bhatia-4080 <gaurav.bhatia2099@gmail.com>
2020-06-02 12:10:01 +0530

    added charts

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
-import React from 'react'
-import {Cards,Chart, CountryPicker} from "./Components"
+import React from 'react';
 
-import styles from './App.module.css'
+import { Cards, CountryPicker, Chart } from './Components';
+import { fetchData } from './api/';
+import styles from './App.module.css';
 
-import {fetchData} from './api'
-class App extends React.Component{
-    state={
-        covidData:{}
-    }
-    async componentDidMount(){
-        const fetchedCovidData = await fetchData()
+// import image from './images/image.png';
 
-        this.setState({covidData:fetchedCovidData})
-    }
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
 
-    render(){
-        const { covidData }= this.state
-        return(
-            <div className={styles.container}>
-                <Cards data={covidData}/>
-                <CountryPicker />
-                <Chart />
-            </div>
-        )
-    }
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        {/* <img className={styles.image} src={image} alt="COVID-19" /> */}
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} /> 
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
--- src/api/index.js
+++ src/api/index.js
@@ -1,25 +1,65 @@
-import axios from 'axios'
+// import axios from 'axios'
 
-const url = "https://covid19.mathdro.id/api"
+// const url = "https://covid19.mathdro.id/api"
 
-export const fetchData = async () => {
-    try{
-        const { data : { confirmed, recovered, deaths, lastUpdate }} = await axios.get(url)
-        // const modifiedData = {
-        //     confirmed: confirmed,
-        //     recovered: recovered,
-        //     deaths: deaths,
-        //     lastUpdate: lastUpdate
-        // }
-        // Same as below
+// export const fetchData = async () => {
+//     try{
+//         const { data : { confirmed, recovered, deaths, lastUpdate }} = await axios.get(url)
+//         // const modifiedData = {
+//         //     confirmed: confirmed,
+//         //     recovered: recovered,
+//         //     deaths: deaths,
+//         //     lastUpdate: lastUpdate
+//         // }
+//         // Same as below
 
-        const modifiedData = { confirmed, recovered, deaths, lastUpdate }
+//         const modifiedData = { confirmed, recovered, deaths, lastUpdate }
         
-        return modifiedData
+//         return modifiedData
 
-    } catch(error){
+//     } catch(error){
 
-        console.log("Error:", error)
+//         console.log("Error:", error)
 
-    }
-}
+//     }
+// }
+
+import axios from 'axios';
+
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async (country) => {
+  let changeableUrl = url;
+
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
+  }
+
+  try {
+    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
+
+    return { confirmed, recovered, deaths, lastUpdate };
+  } catch (error) {
+    return error;
+  }
+};
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+
+    return data.map(({ confirmed, deaths, reportDate: date }) => ({ confirmed: confirmed.total, deaths: deaths.total, date }));
+  } catch (error) {
+    return error;
+  }
+};
+
+export const fetchCountries = async () => {
+  try {
+    const { data: { countries } } = await axios.get(`${url}/countries`);
+
+    return countries.map((country) => country.name);
+  } catch (error) {
+    return error;
+  }
+};
